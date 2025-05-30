18a6d32374bec77f07a96be56196289d34c53406
Verryx-02 <francescoverrengia62442@gmail.com>
2025-05-30 22:25:30 +0200

    Inserito dei commenti piu' chiari per ogni file. Cambiato il nome della cartella clients->interfaces

--- https_server/src/types/types.go
+++ https_server/src/types/types.go
@@ -6,14 +6,16 @@ used throughout the application for consistent data handling.
 
 package types
 
-// Registration request structure
+// RegisterRequest represents the JSON that the Entry-Hub sends to the Security-Switch during the user registration phase.
+// This type is serialized in JSON and sent via an HTTPS POST request with mTLS authentication.
+// The Security-Switch uses this information to register the user in the system:
 type RegisterRequest struct {
 	Email     string `json:"email"`
 	Password  string `json:"password"`
 	SSHPubKey string `json:"ssh_public_key"`
 }
 
-// Login request structure.
+// Login request structure. WARNING: The login is not implemented yet.
 /*
 type LoginRequest struct {
 	Email    string `json:"email"` // email hashed by the client using Argon2id
@@ -21,7 +23,8 @@ type LoginRequest struct {
 }
 */
 
-// Structure to represent a user
+// WARNING:This User structure is not used anymore. It was used ad a test.
+// It's here because I don't want to throw away some code for now.
 type User struct {
 	Email        string `json:"email"`
 	PasswordHash string `json:"password_hash"`
@@ -29,7 +32,9 @@ type User struct {
 	SSHPubKey    string `json:"ssh_public_key"`
 }
 
-// Response structure
+// Response represents the standard response structure sent by the Entry-Hub to the Client and by the Security-Switch to the Entry-Hub
+// following a registration or authentication request.
+// This structure is serialized in JSON and sent as an HTTP response.
 type Response struct {
 	Success bool   `json:"success"`
 	Message string `json:"message"`
